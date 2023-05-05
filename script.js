933197c7250628235f880f2225f991cf1b194125
rovedabr <rovedabr@gmail.com>
2023-05-05 13:25:19 -0300

    styles min with 1280px - started background

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+buttonChangeMode = document.querySelector("#modeIcon")
+
 function goNext() {
   return window.scrollBy(400, 700);
 } 
+
+function changeMode() {
+  document.body.classList.toggle("dark")
+}
 
